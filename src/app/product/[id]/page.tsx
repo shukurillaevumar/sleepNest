780334717e7e7fcb632d23fd4ffd11d02e847330
Muhammadumar <khilolamoon@gmail.com>
2025-04-20 22:16:59 +0500

    fix 3

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,12 +1,12 @@
 import { products } from "../../data/products";
-import ProductDetails from "./ProductDetails"; // Клиентский компонент
+import ProductDetails from "./ProductDetails";
 
 interface ProductProps {
-  params: { id: string };
+  params: { id: string }; // ✅ без Promise
 }
 
-export default async function ProductPage(props: ProductProps) {
-  const { id } = await props.params;
+export default async function ProductPage(props: any) {
+  const id = (await props?.params)?.id;
   const product = products.find((p) => p.id === parseInt(id));
 
   if (!product) {
