93137d964fce80aca190f518be7d1ca2c603b3ff
Muhammadumar <khilolamoon@gmail.com>
2025-04-20 22:05:42 +0500

    bug fixed

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -6,7 +6,7 @@ interface ProductProps {
 }
 
 export default async function ProductPage({ params }: ProductProps) {
-  const { id } = params;
+  const { id } = await params;
   const product = products.find((p) => p.id === parseInt(id));
 
   if (!product) {
